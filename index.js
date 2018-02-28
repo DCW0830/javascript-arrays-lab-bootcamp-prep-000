283c1d49dbe7aefdee68f3843e7028e3365d54ae
Daniel Walz <walzdan@gmail.com>
2018-02-28 18:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-Var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ['Milo', 'Otis', 'Garfield']
+
 const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   kittens.shift(ralph)
