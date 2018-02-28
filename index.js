2ecdf23fda3297423eff2471023c7786ac8cad40
Daniel Walz <walzdan@gmail.com>
2018-02-28 18:49:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 const app = "I don't do much."
+
 function destructivelyAppendKitten(name) {
   kittens.push("Ralph")
   return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshit()
 }
